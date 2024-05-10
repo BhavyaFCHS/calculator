50fcd31294865f68c2f6d0fd2ef08873826ad1ce
Bhavya Chaudhary <chaudhary.bhavya2005@gmail.com>
2024-05-10 19:47:21 -0400

    created objects for numbers and operations

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
 // DOM Elements
-const numbers = document.querySelector(".number-container");
-const operations = document.querySelector(".operations-container");
+const numpad = document.querySelector(".number-container");
+const operationsPad = document.querySelector(".operations-container");
 const output = document.querySelector(".display");
 
+// Calculator objects
+const numbers = {
+    one : 1, 
+    two : 2, 
+    three : 3, 
+    four : 4,
+    five : 5,
+    six : 6,
+    seven : 7,
+    eight : 8,
+    nine : 9,
+    zero : 0,
+};
+
+const operations = {
+    add : "+", 
+    subtract : "-", 
+    multiply : "*", 
+    divide : "/",
+};
+
+// Array storing the current calculation
+let calculation = "";
+
 // Event Handlers
-numbers.addEventListener("click", e => {
-    let num = e.target.textContent;
-    output.textContent = num;
+numpad.addEventListener("click", e => {
+    let num = e.target.id;
+    if(num in numbers) {
+        calculation += numbers[num];
+    }
+    output.textContent = calculation;
 });
 
-operations.addEventListener("click", e => {
-    let operation = e.target.textContent;
-    output.textContent = operation;
+operationsPad.addEventListener("click", e => {
+    let operation = e.target.id;
+    if(operation in operations) {
+        calculation += operations[operation];
+    }
+    output.textContent = calculation;
 });
